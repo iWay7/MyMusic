b4ede5c9506dc4959ae686985ae6b52265d8aa43
iWay <806070587@qq.com>
2018-05-29 19:11:57 +0800

    Changes.

---
+++ MyMusic-Android/app/src/main/java/site/iway/mymusic/user/receivers/HeadsetPlugReceiver.java
@@ -0,0 +1,22 @@
+package site.iway.mymusic.user.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import site.iway.mymusic.utils.Player;
+
+public class HeadsetPlugReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.hasExtra("state")) {
+            int state = intent.getIntExtra("state", 0);
+            if (state == 0) {
+                Player player = Player.getInstance();
+                player.pause();
+            }
+        }
+    }
+
+}
