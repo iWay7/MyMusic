d46ac67bf0450b8cb5f03bf449bc5c1db5bc4abb
iWay7 <806070587@qq.com>
2018-06-08 15:30:44 +0800

    Changes.

--- MyMusic-Server/src/site/iway/mymusic/config/Environment.java
+++ MyMusic-Server/src/site/iway/mymusic/config/Environment.java
@@ -4,5 +4,6 @@ public class Environment {
 
     public static final String MUSIC_ROOT = "D:\\Music\\";
     public static final String PLAY_LIST = "C:\\Users\\iWay\\.play_list";
+    public static final String LYRIC_ROOT = "C:\\Users\\iWay\\.lyrics\\";
 
 }
--- MyMusic-Server/src/site/iway/mymusic/servlets/GetSongInfoServlet.java
+++ MyMusic-Server/src/site/iway/mymusic/servlets/GetSongInfoServlet.java
@@ -1,7 +1,9 @@
 package site.iway.mymusic.servlets;
 
+import site.iway.mymusic.config.Environment;
 import site.iway.mymusic.external.baidu.BaiduMusicResponse;
 import site.iway.mymusic.external.baidu.BaiduSongListItem;
+import site.iway.mymusic.protocol.data.Song;
 import site.iway.mymusic.protocol.data.SongInfo;
 import site.iway.mymusic.protocol.res.GetSongInfoRes;
 import site.iway.mymusic.protocol.res.RPCRes;
@@ -9,23 +11,17 @@ import site.iway.mymusic.protocol.res.RPCRes;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 public class GetSongInfoServlet extends BasicServlet {
-
-    private static ConcurrentMap<String, GetSongInfoRes> sResultCache = new ConcurrentHashMap<>();
 
     @Override
     protected void doRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String query = req.getParameter("query");
-        if (sResultCache.containsKey(query)) {
-            putJSONResponse(resp, sResultCache.get(query));
-            return;
-        }
+        String fileName = req.getParameter("fileName");
         String url = "http://tingapi.ting.baidu.com/v1/restserver/ting?" +
                 "from=android&" +
                 "version=5.6.5.0&" +
@@ -39,12 +35,18 @@ public class GetSongInfoServlet extends BasicServlet {
                 "use_cluster=1";
         BaiduMusicResponse response = readFromURLAsObject(url, BaiduMusicResponse.class);
         GetSongInfoRes getSongInfoRes = new GetSongInfoRes();
+        getSongInfoRes.list = new ArrayList<>();
+        if (new File(Environment.LYRIC_ROOT, fileName).exists()) {
+            SongInfo songInfo = new SongInfo();
+            songInfo.imgLink = null;
+            Song song = new Song(fileName);
+            songInfo.lrcTitle = song.artist + " - " + song.name;
+            songInfo.lrcLink = "MY";
+            getSongInfoRes.list.add(songInfo);
+        }
         if (response != null && response.error_code == 22000 &&
                 response.result != null && response.result.song_info != null &&
-                response.result.song_info.song_list != null && !response.result.song_info.song_list.isEmpty()) {
-            getSongInfoRes.resultCode = RPCRes.OK;
-            getSongInfoRes.resultMessage = RPCRes.OK_STRING;
-            getSongInfoRes.list = new ArrayList<>();
+                response.result.song_info.song_list != null) {
             for (BaiduSongListItem item : response.result.song_info.song_list) {
                 SongInfo songInfo = new SongInfo();
                 songInfo.imgLink = item.pic_small;
@@ -52,11 +54,9 @@ public class GetSongInfoServlet extends BasicServlet {
                 songInfo.lrcLink = item.lrclink;
                 getSongInfoRes.list.add(songInfo);
             }
-        } else {
-            getSongInfoRes.resultCode = RPCRes.FAIL;
-            getSongInfoRes.resultMessage = RPCRes.FAIL_STRING;
         }
-        sResultCache.put(query, getSongInfoRes);
+        getSongInfoRes.resultCode = RPCRes.OK;
+        getSongInfoRes.resultMessage = RPCRes.OK_STRING;
         putJSONResponse(resp, getSongInfoRes);
     }
 
